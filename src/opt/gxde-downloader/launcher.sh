14d48e826bfd420ffb46cfd100afb93b3cfb2337
shenmo <jifengshenmo@outlook.com>
2024-10-20 09:46:55 +0000

    update src/opt/gxde-downloader/launcher.sh.
    Signed-off-by: shenmo <jifengshenmo@outlook.com>

--- src/opt/gxde-downloader/launcher.sh
+++ src/opt/gxde-downloader/launcher.sh
@@ -6,7 +6,8 @@
 
 D_DTK_SIZEMODE=1 spark-webapp-runtime -u "file:///opt/gxde-downloader/index.html" --tray --hide-buttons --title="GXDE Downloader"  --desc="GXDE Downloader is a frontend of Aria2c, Using AriaNG" --ico=/opt/gxde-downloader/downloader-arrow.svg 
 
-if [[ ! -e "/tmp/_d_dtk_single_instance_1000_GXDE Downloader_1000" ]];then
-# Only kill if all the instance
-killall gxdown-aria2-launcher.sh || killall gxdown-aria2-launcher.sh
+
+if [ ! -e "/tmp/_d_dtk_single_instance_${UID}_GXDE Downloader_${UID}" ] ; then
+	# Only kill if all the instance
+	killall gxdown-aria2-launcher.sh || true
 fi
